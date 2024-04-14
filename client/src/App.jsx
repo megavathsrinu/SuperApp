69abbf3d9e714028b9e2d7c16ca3836b1850771c
megavathsrinu <megavathsrinu2002@gmail.com>
2024-04-14 14:12:50 +0530

    Fourth commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Genre from "./pages/Genre";
+import Movies from "./pages/Movies";
+import Browse from "./pages/Browse";
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/genre" element={<Genre />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
